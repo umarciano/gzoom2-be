5af29743e05c32801168406d1f2a6c83fceb08ea
Andrea Fossi <andrea.fossi@mapsgroup.it>
2018-09-04 11:40:26 +0200

    Fixed compilation bugs

--- report-persistence-query-dsl/src/test/java/it/mapsgroup/gzoom/report/querydsl/dao/AbstractReportDaoIT.java
+++ report-persistence-query-dsl/src/test/java/it/mapsgroup/gzoom/report/querydsl/dao/AbstractReportDaoIT.java
@@ -1,7 +1,6 @@
 package it.mapsgroup.gzoom.report.querydsl.dao;
 
 import it.mapsgroup.gzoom.persistence.common.CommonPersistenceConfiguration;
-import it.mapsgroup.gzoom.report.querydsl.persistence.service.ReportQueryDslPersistenceConfiguration;
 import org.junit.runner.RunWith;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,7 +12,7 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
  * @author Andrea Fossi.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = {CommonPersistenceConfiguration.class, ReportQueryDslPersistenceConfiguration.class})
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = {CommonPersistenceConfiguration.class/*, ReportQueryDslPersistenceConfiguration.class*/})
 //@ComponentScan("it.mapsgroup.gzoom.report.querydsl.dao")
 @ComponentScan(basePackageClasses = {ReportActivityDao.class})
 @TestPropertySource("/gzoom.properties")
